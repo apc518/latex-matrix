85c6fa73c9e65788b88129b6ff4e227953e91ff7
Andy <apc518@ymail.com>
2021-10-23 14:26:53 -0700

    mathjax lib runs after my main.js

--- main.js
+++ main.js
@@ -270,7 +270,6 @@ const genLatex = () => {
     document.getElementById("latex").value = latex;
     document.getElementById("preview").innerHTML = latex;
     MathJax.typeset();
-    setTimeout(() => MathJax.typeset(), 0); // ensure that the typesetting runs with everything loaded properly by deferring it until the stack is empty
     
     let previewWrapperWidth = Math.max(250, document.getElementsByClassName('MJX-TEX')[0].clientWidth + 60);
     let previewWrapperHeight = Math.max(150, document.getElementsByClassName('MJX-TEX')[0].clientHeight + 60);
